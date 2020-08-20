4f6f8abf68fc26e12e120ebbb7c082b981aa6cc4
Andy Armstrong <andy@hexten.net>
2020-08-20 14:13:12 +0100

    Minor

--- test/conformance.js
+++ test/conformance.js
@@ -70,15 +70,19 @@ const paths = [
   //  "$..[(@.length-1)]", // All last elements
 ];
 
-for (const path of paths) {
-  for (const go of [1, 2]) {
+for (const attempt of [1, 2]) {
+  for (const path of paths) {
     for (const method of ["query", "paths", "nodes"]) {
       // TODO zero case doesn't work for us
       for (const count of [undefined, /* 0, */ 1, 3]) {
         const got = jpc[method](obj, path, count);
         const want = jp[method](obj, path, count);
         const cd = count === undefined ? "∞" : count;
-        tap.same(got, want, `go ${go}: ${method} of ${path} (count: ${cd})`);
+        tap.same(
+          got,
+          want,
+          `attempt ${attempt}: ${method} of ${path} (count: ${cd})`
+        );
       }
     }
   }
