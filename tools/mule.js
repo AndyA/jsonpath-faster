3759917eabb8fac23d97d285c5e01697825e4dcf
Andy Armstrong <andy@hexten.net>
2020-08-24 16:49:35 +0100

    MultiPath

--- tools/mule.js
+++ tools/mule.js
@@ -1,29 +1,19 @@
 "use strict";
 
 const prettier = require("prettier");
-const { makeTree, mergeTrees, renderTree } = require("../lib/merge");
-const { inspect } = require("../lib/util");
+
 const jp = require("..");
+const { inspect } = require("../lib/util");
+const { MultiPath } = require("../lib/multipath");
 
 const exprs = ["$.foo.bar", "$.foo.baz[0]", "$.foo.baz[1]", "$.foo..id"];
 
-const makeOp = expr => [
-  ...jp.parse(expr),
-  {
-    scope: "internal",
-    operation: "terminal",
-    lastly: ctx => `console.log(${JSON.stringify(expr)}, path);`
-  }
-];
+const mp = new MultiPath();
 
-const out = [];
-for (const expr of exprs) {
-  mergeTrees(out, makeTree(makeOp(expr)));
-}
-//console.log(inspect(out));
+for (const expr of exprs)
+  mp.addAction(expr, ctx => `console.log(${JSON.stringify(expr)}, path);`);
 
-const ast = renderTree(out);
-const code = jp.compiler.compileTokens(ast, { trackPath: true });
-//console.log(code);
+const code = jp.compiler.compileTokens(mp.render(), { trackPath: true });
+
 const pretty = prettier.format(code, { filepath: "code.js" });
 console.log(pretty);
