610594e83d87d04bdf0c84e5a59b1e8e4acf1568
Andy Armstrong <andy@hexten.net>
2020-08-24 15:52:18 +0100

    Experiments

---
+++ tools/template.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const { inspect } = require("../lib/util");
+
+const jp = (strings, ...key) => {
+  return {
+    nodes(...args) {
+      return { strings, key, args };
+    }
+  };
+};
+
+const i = 12;
+const j = 3;
+const obj = {};
+const x = jp`$.names[${i}][${j}]..id`.nodes(obj);
+
+console.log(inspect(x));
+
+process.exit(1);
+
+// I'd like this to work. Think it can. However the appeal is
+// limited to literal paths.
+jp`$.names[${i}][${j}]..id`.each(obj, (value, path) => {});
+
+// This would also be nice.
+const v = jp`$.name[0][0].id`.value(obj);
