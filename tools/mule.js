16dd70e6832eeb3d23a155f45e8909fdc5d88fda
Andy Armstrong <andy@hexten.net>
2020-08-24 15:52:31 +0100

    Merging trees

--- tools/mule.js
+++ tools/mule.js
@@ -1,12 +1,29 @@
 "use strict";
 
-const esprima = require("esprima");
-const estraverse = require("estraverse");
-const escodegen = require("escodegen");
+const prettier = require("prettier");
+const { makeTree, mergeTrees, renderTree } = require("../lib/merge");
 const { inspect } = require("../lib/util");
+const jp = require("..");
 
-const exprs = ["hit", "hit.miss"];
+const exprs = ["$.foo.bar", "$.foo.baz[0]", "$.foo.baz[1]", "$.foo..id"];
+
+const makeOp = expr => [
+  ...jp.parse(expr),
+  {
+    scope: "internal",
+    operation: "terminal",
+    lastly: ctx => `console.log(${JSON.stringify(expr)}, path);`
+  }
+];
+
+const out = [];
 for (const expr of exprs) {
-  console.log(`// ${expr}`);
-  console.log(inspect(esprima.parse(expr)));
+  mergeTrees(out, makeTree(makeOp(expr)));
 }
+//console.log(inspect(out));
+
+const ast = renderTree(out);
+const code = jp.compiler.compileTokens(ast, { trackPath: true });
+//console.log(code);
+const pretty = prettier.format(code, { filepath: "code.js" });
+console.log(pretty);
