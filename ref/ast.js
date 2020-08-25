76c7a47f683947b0cfdf54b48702798f26986372
Andy Armstrong <andy@hexten.net>
2020-08-25 11:50:16 +0100

    Recreate ref ast.

--- ref/ast.js
+++ ref/ast.js
@@ -1,216 +1,14 @@
 module.exports = [
   {
-    path: "$.store",
+    path: "$.*",
     ast: [
       { expression: { type: "root", value: "$" } },
-      {
-        expression: { type: "identifier", value: "store" },
-        scope: "child",
-        operation: "member"
-      }
-    ]
-  },
-  {
-    path: "$.store.bicycle",
-    ast: [
-      { expression: { type: "root", value: "$" } },
-      {
-        expression: { type: "identifier", value: "store" },
-        scope: "child",
-        operation: "member"
-      },
-      {
-        expression: { type: "identifier", value: "bicycle" },
-        scope: "child",
-        operation: "member"
-      }
-    ]
-  },
-  {
-    path: "$.store.*",
-    ast: [
-      { expression: { type: "root", value: "$" } },
-      {
-        expression: { type: "identifier", value: "store" },
-        scope: "child",
-        operation: "member"
-      },
       {
         expression: { type: "wildcard", value: "*" },
         scope: "child",
-        operation: "member"
-      }
-    ]
-  },
-  {
-    path: "$.store[*]",
-    ast: [
-      { expression: { type: "root", value: "$" } },
-      {
-        expression: { type: "identifier", value: "store" },
-        scope: "child",
-        operation: "member"
+        operation: "member",
       },
-      {
-        expression: { type: "wildcard", value: "*" },
-        scope: "child",
-        operation: "subscript"
-      }
-    ]
-  },
-  {
-    path: "$.store.book[1]",
-    ast: [
-      { expression: { type: "root", value: "$" } },
-      {
-        expression: { type: "identifier", value: "store" },
-        scope: "child",
-        operation: "member"
-      },
-      {
-        expression: { type: "identifier", value: "book" },
-        scope: "child",
-        operation: "member"
-      },
-      {
-        expression: { type: "numeric_literal", value: 1 },
-        scope: "child",
-        operation: "subscript"
-      }
-    ]
-  },
-  {
-    path: "$.store.book.1",
-    ast: [
-      { expression: { type: "root", value: "$" } },
-      {
-        expression: { type: "identifier", value: "store" },
-        scope: "child",
-        operation: "member"
-      },
-      {
-        expression: { type: "identifier", value: "book" },
-        scope: "child",
-        operation: "member"
-      },
-      {
-        expression: { type: "numeric_literal", value: 1 },
-        scope: "child",
-        operation: "member"
-      }
-    ]
-  },
-  {
-    path: '$.store.bicycle["color"]',
-    ast: [
-      { expression: { type: "root", value: "$" } },
-      {
-        expression: { type: "identifier", value: "store" },
-        scope: "child",
-        operation: "member"
-      },
-      {
-        expression: { type: "identifier", value: "bicycle" },
-        scope: "child",
-        operation: "member"
-      },
-      {
-        expression: { type: "string_literal", value: "color" },
-        scope: "child",
-        operation: "subscript"
-      }
-    ]
-  },
-  {
-    path: "$.store.book[*].author",
-    ast: [
-      { expression: { type: "root", value: "$" } },
-      {
-        expression: { type: "identifier", value: "store" },
-        scope: "child",
-        operation: "member"
-      },
-      {
-        expression: { type: "identifier", value: "book" },
-        scope: "child",
-        operation: "member"
-      },
-      {
-        expression: { type: "wildcard", value: "*" },
-        scope: "child",
-        operation: "subscript"
-      },
-      {
-        expression: { type: "identifier", value: "author" },
-        scope: "child",
-        operation: "member"
-      }
-    ]
-  },
-  {
-    path: "$..author",
-    ast: [
-      { expression: { type: "root", value: "$" } },
-      {
-        expression: { type: "identifier", value: "author" },
-        scope: "descendant",
-        operation: "member"
-      }
-    ]
-  },
-  {
-    path: "$..[1]",
-    ast: [
-      { expression: { type: "root", value: "$" } },
-      {
-        expression: { type: "numeric_literal", value: 1 },
-        scope: "descendant",
-        operation: "subscript"
-      }
-    ]
-  },
-  {
-    path: "$..[(@.length-1)]",
-    ast: [
-      { expression: { type: "root", value: "$" } },
-      {
-        expression: { type: "script_expression", value: "(@.length-1)" },
-        scope: "descendant",
-        operation: "subscript"
-      }
-    ]
-  },
-  {
-    path: "$.store..price",
-    ast: [
-      { expression: { type: "root", value: "$" } },
-      {
-        expression: { type: "identifier", value: "store" },
-        scope: "child",
-        operation: "member"
-      },
-      {
-        expression: { type: "identifier", value: "price" },
-        scope: "descendant",
-        operation: "member"
-      }
-    ]
-  },
-  {
-    path: "$..book[2]",
-    ast: [
-      { expression: { type: "root", value: "$" } },
-      {
-        expression: { type: "identifier", value: "book" },
-        scope: "descendant",
-        operation: "member"
-      },
-      {
-        expression: { type: "numeric_literal", value: 2 },
-        scope: "child",
-        operation: "subscript"
-      }
-    ]
+    ],
   },
   {
     path: "$..*",
@@ -219,41 +17,211 @@ module.exports = [
       {
         expression: { type: "wildcard", value: "*" },
         scope: "descendant",
-        operation: "member"
-      }
-    ]
+        operation: "member",
+      },
+    ],
   },
   {
-    path: "$..book[(@.length-1)]",
+    path: "$.store",
+    ast: [
+      { expression: { type: "root", value: "$" } },
+      {
+        expression: { type: "identifier", value: "store" },
+        scope: "child",
+        operation: "member",
+      },
+    ],
+  },
+  {
+    path: "$.store.bicycle",
+    ast: [
+      { expression: { type: "root", value: "$" } },
+      {
+        expression: { type: "identifier", value: "store" },
+        scope: "child",
+        operation: "member",
+      },
+      {
+        expression: { type: "identifier", value: "bicycle" },
+        scope: "child",
+        operation: "member",
+      },
+    ],
+  },
+  {
+    path: '$.store.bicycle["color"]',
+    ast: [
+      { expression: { type: "root", value: "$" } },
+      {
+        expression: { type: "identifier", value: "store" },
+        scope: "child",
+        operation: "member",
+      },
+      {
+        expression: { type: "identifier", value: "bicycle" },
+        scope: "child",
+        operation: "member",
+      },
+      {
+        expression: { type: "string_literal", value: "color" },
+        scope: "child",
+        operation: "subscript",
+      },
+    ],
+  },
+  {
+    path: "$.store.*",
+    ast: [
+      { expression: { type: "root", value: "$" } },
+      {
+        expression: { type: "identifier", value: "store" },
+        scope: "child",
+        operation: "member",
+      },
+      {
+        expression: { type: "wildcard", value: "*" },
+        scope: "child",
+        operation: "member",
+      },
+    ],
+  },
+  {
+    path: "$.store[*]",
+    ast: [
+      { expression: { type: "root", value: "$" } },
+      {
+        expression: { type: "identifier", value: "store" },
+        scope: "child",
+        operation: "member",
+      },
+      {
+        expression: { type: "wildcard", value: "*" },
+        scope: "child",
+        operation: "subscript",
+      },
+    ],
+  },
+  {
+    path: "$.store.book[1]",
+    ast: [
+      { expression: { type: "root", value: "$" } },
+      {
+        expression: { type: "identifier", value: "store" },
+        scope: "child",
+        operation: "member",
+      },
+      {
+        expression: { type: "identifier", value: "book" },
+        scope: "child",
+        operation: "member",
+      },
+      {
+        expression: { type: "numeric_literal", value: 1 },
+        scope: "child",
+        operation: "subscript",
+      },
+    ],
+  },
+  {
+    path: "$.store.book.1",
+    ast: [
+      { expression: { type: "root", value: "$" } },
+      {
+        expression: { type: "identifier", value: "store" },
+        scope: "child",
+        operation: "member",
+      },
+      {
+        expression: { type: "identifier", value: "book" },
+        scope: "child",
+        operation: "member",
+      },
+      {
+        expression: { type: "numeric_literal", value: 1 },
+        scope: "child",
+        operation: "member",
+      },
+    ],
+  },
+  {
+    path: "$.store.book[*].author",
+    ast: [
+      { expression: { type: "root", value: "$" } },
+      {
+        expression: { type: "identifier", value: "store" },
+        scope: "child",
+        operation: "member",
+      },
+      {
+        expression: { type: "identifier", value: "book" },
+        scope: "child",
+        operation: "member",
+      },
+      {
+        expression: { type: "wildcard", value: "*" },
+        scope: "child",
+        operation: "subscript",
+      },
+      {
+        expression: { type: "identifier", value: "author" },
+        scope: "child",
+        operation: "member",
+      },
+    ],
+  },
+  {
+    path: "$..author",
+    ast: [
+      { expression: { type: "root", value: "$" } },
+      {
+        expression: { type: "identifier", value: "author" },
+        scope: "descendant",
+        operation: "member",
+      },
+    ],
+  },
+  {
+    path: "$..[1]",
+    ast: [
+      { expression: { type: "root", value: "$" } },
+      {
+        expression: { type: "numeric_literal", value: 1 },
+        scope: "descendant",
+        operation: "subscript",
+      },
+    ],
+  },
+  {
+    path: "$.store..price",
+    ast: [
+      { expression: { type: "root", value: "$" } },
+      {
+        expression: { type: "identifier", value: "store" },
+        scope: "child",
+        operation: "member",
+      },
+      {
+        expression: { type: "identifier", value: "price" },
+        scope: "descendant",
+        operation: "member",
+      },
+    ],
+  },
+  {
+    path: "$..book[2]",
     ast: [
       { expression: { type: "root", value: "$" } },
       {
         expression: { type: "identifier", value: "book" },
         scope: "descendant",
-        operation: "member"
+        operation: "member",
       },
       {
-        expression: { type: "script_expression", value: "(@.length-1)" },
+        expression: { type: "numeric_literal", value: 2 },
         scope: "child",
-        operation: "subscript"
-      }
-    ]
-  },
-  {
-    path: "$..book[-1:]",
-    ast: [
-      { expression: { type: "root", value: "$" } },
-      {
-        expression: { type: "identifier", value: "book" },
-        scope: "descendant",
-        operation: "member"
+        operation: "subscript",
       },
-      {
-        expression: { type: "slice", value: "-1:" },
-        scope: "child",
-        operation: "subscript"
-      }
-    ]
+    ],
   },
   {
     path: "$..book[0,1]",
@@ -262,20 +230,36 @@ module.exports = [
       {
         expression: { type: "identifier", value: "book" },
         scope: "descendant",
-        operation: "member"
+        operation: "member",
       },
       {
         expression: {
           type: "union",
           value: [
             { expression: { type: "numeric_literal", value: 0 } },
-            { expression: { type: "numeric_literal", value: 1 } }
-          ]
+            { expression: { type: "numeric_literal", value: 1 } },
+          ],
         },
         scope: "child",
-        operation: "subscript"
-      }
-    ]
+        operation: "subscript",
+      },
+    ],
+  },
+  {
+    path: "$..book[-1:]",
+    ast: [
+      { expression: { type: "root", value: "$" } },
+      {
+        expression: { type: "identifier", value: "book" },
+        scope: "descendant",
+        operation: "member",
+      },
+      {
+        expression: { type: "slice", value: "-1:" },
+        scope: "child",
+        operation: "subscript",
+      },
+    ],
   },
   {
     path: "$..book[:2]",
@@ -284,14 +268,30 @@ module.exports = [
       {
         expression: { type: "identifier", value: "book" },
         scope: "descendant",
-        operation: "member"
+        operation: "member",
       },
       {
         expression: { type: "slice", value: ":2" },
         scope: "child",
-        operation: "subscript"
-      }
-    ]
+        operation: "subscript",
+      },
+    ],
+  },
+  {
+    path: "$..book[(@.length-1)]",
+    ast: [
+      { expression: { type: "root", value: "$" } },
+      {
+        expression: { type: "identifier", value: "book" },
+        scope: "descendant",
+        operation: "member",
+      },
+      {
+        expression: { type: "script_expression", value: "(@.length-1)" },
+        scope: "child",
+        operation: "subscript",
+      },
+    ],
   },
   {
     path: "$..book[?(@.isbn)]",
@@ -300,14 +300,14 @@ module.exports = [
       {
         expression: { type: "identifier", value: "book" },
         scope: "descendant",
-        operation: "member"
+        operation: "member",
       },
       {
         expression: { type: "filter_expression", value: "?(@.isbn)" },
         scope: "child",
-        operation: "subscript"
-      }
-    ]
+        operation: "subscript",
+      },
+    ],
   },
   {
     path: "$..book[?(@.price<10)]",
@@ -316,14 +316,14 @@ module.exports = [
       {
         expression: { type: "identifier", value: "book" },
         scope: "descendant",
-        operation: "member"
+        operation: "member",
       },
       {
         expression: { type: "filter_expression", value: "?(@.price<10)" },
         scope: "child",
-        operation: "subscript"
-      }
-    ]
+        operation: "subscript",
+      },
+    ],
   },
   {
     path: "$..book[?(@.price==8.95)]",
@@ -332,14 +332,14 @@ module.exports = [
       {
         expression: { type: "identifier", value: "book" },
         scope: "descendant",
-        operation: "member"
+        operation: "member",
       },
       {
         expression: { type: "filter_expression", value: "?(@.price==8.95)" },
         scope: "child",
-        operation: "subscript"
-      }
-    ]
+        operation: "subscript",
+      },
+    ],
   },
   {
     path: '$..book[?(@.price<30 && @.category=="fiction")]',
@@ -348,16 +348,28 @@ module.exports = [
       {
         expression: { type: "identifier", value: "book" },
         scope: "descendant",
-        operation: "member"
+        operation: "member",
       },
       {
         expression: {
           type: "filter_expression",
-          value: '?(@.price<30 && @.category=="fiction")'
+          value: '?(@.price<30 && @.category=="fiction")',
         },
         scope: "child",
-        operation: "subscript"
-      }
-    ]
-  }
+        operation: "subscript",
+      },
+    ],
+  },
+  {
+    path: "$..[(@.length-1)]",
+    ast: [
+      { expression: { type: "root", value: "$" } },
+      {
+        expression: { type: "script_expression", value: "(@.length-1)" },
+        scope: "descendant",
+        operation: "subscript",
+      },
+    ],
+  },
 ];
+
---
+++ tools/ast.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const jp = require("..");
+const prettier = require("prettier");
+
+const paths = [
+  "$.*",
+  "$..*", // All members of JSON structure
+  "$.store",
+  "$.store.bicycle",
+  '$.store.bicycle["color"]',
+  "$.store.*", // All things in store, which are some books and a red bicycle
+  "$.store[*]", // All things in store, which are some books and a red bicycle
+  "$.store.book[1]",
+  "$.store.book.1",
+  "$.store.book[*].author", // The authors of all books in the store
+  "$..author", // All authors
+  "$..[1]", // All second elements
+  "$.store..price", // The price of everything in the store
+  "$..book[2]", // The third book
+  "$..book[0,1]", // The first two books via subscript union
+  "$..book[-1:]", // The last book via slice
+  "$..book[:2]", // The first two books via subscript array slice
+  "$..book[(@.length-1)]", // The last book via script subscript
+  "$..book[?(@.isbn)]", // Filter all books with isbn number
+  "$..book[?(@.price<10)]", // Filter all books cheaper than 10
+  "$..book[?(@.price==8.95)]", // Filter all books that cost 8.95
+  '$..book[?(@.price<30 && @.category=="fiction")]', // Filter all fiction books cheaper than 30
+
+  //  jsonpath can't handle this
+  "$..[(@.length-1)]" // All last elements
+];
+
+const stash = [];
+
+for (const path of paths) {
+  const ast = jp.parse(path);
+  stash.push({ path, ast });
+}
+
+const pretty = prettier.format(`module.exports = ${JSON.stringify(stash)}`, {
+  filepath: "code.js"
+});
+console.log(pretty);
