de1d74aacec8254e5c6f124bd4cfa55f7499b163
Andy Armstrong <andy@hexten.net>
2020-08-25 20:16:59 +0100

    Syntax tests.

--- test/lastly.js
+++ test/lastly.js
@@ -3,20 +3,32 @@
 const tap = require("tap");
 const { bindLastly } = require("../lib/lastly");
 
-const expr = bindLastly("@.pathString", {
-  path: () => "path",
-  pathString: () => "jp.stringify(@.path)"
+tap.test(`recursive`, async () => {
+  const expr = bindLastly("@.pathString", {
+    path: () => "path",
+    pathString: () => "jp.stringify(@.path)"
+  });
+  tap.equals(expr, "jp.stringify(path)", `recursive resolution`);
 });
-tap.equals(expr, "jp.stringify(path)", `recursive resolution`);
 
-tap.throws(
-  () => bindLastly("@", {}),
-  /direct access/i,
-  `No direct access to context`
-);
+tap.test(`syntax`, async () => {
+  const expr = bindLastly("@.pathString.length", {
+    path: () => "path",
+    pathString: () => "jp.stringify(@.path)"
+  });
+  tap.equals(expr, "jp.stringify(path).length", `syntax`);
+});
 
-tap.throws(
-  () => bindLastly("@.foo", {}),
-  /has no.*foo/i,
-  `Missing from context`
-);
+tap.test(`negative`, async () => {
+  tap.throws(
+    () => bindLastly("@", {}),
+    /direct access/i,
+    `No direct access to context`
+  );
+
+  tap.throws(
+    () => bindLastly("@.foo", {}),
+    /has no.*foo/i,
+    `Missing from context`
+  );
+});
