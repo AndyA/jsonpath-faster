6414dd3de6ec01dd87af521dc4fb0d90604c43ff
Andy Armstrong <andy@hexten.net>
2020-08-25 19:29:06 +0100

    Magic context in lastly code.

--- tools/mule.js
+++ tools/mule.js
@@ -1,28 +1,10 @@
 "use strict";
 
-const prettier = require("prettier");
+const { bindLastly } = require("../lib/lastly");
 
-const jp = require("..");
-const inspect = require("../lib/inspect");
-const { MultiPath } = require("../lib/multipath");
-const { obj } = require("../benchmark/spec");
-
-const paths = ["$.store", "$.store.bicycle"];
-
-const mp = new MultiPath();
-
-for (const expr of paths)
-  mp.addVisitor(expr, (value, path) => {
-    console.log(`${expr}, ${jp.stringify(path)}: ${value}`);
-  });
-
-if (1) {
-  const code = mp.code();
-  const pretty = prettier.format(`module.exports = ${code}`, {
-    filepath: "code.js"
-  });
-  console.log(pretty);
-}
-
-//const fun = mp.compile();
-//fun(obj);
+const out = bindLastly("cb(@.value, @.path, @.pathString)", {
+  value: () => "obj.foo",
+  path: () => "stack.slice(0)",
+  pathString: () => "jp.stringify(@.path)"
+});
+console.log(out);
