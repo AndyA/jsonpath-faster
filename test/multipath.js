22a11f79849ba5e14fd507a809385f20bdfe68a3
Andy Armstrong <andy@hexten.net>
2020-08-25 17:51:29 +0100

    Coverage for mutators.

--- test/multipath.js
+++ test/multipath.js
@@ -4,14 +4,57 @@ const tap = require("tap");
 const jp = require("..");
 const { MultiPath } = require("../lib/multipath");
 
-const obj = require("./upstream/data/store");
-const paths = require("./data/paths");
+tap.test(`conformance`, async () => {
+  const obj = require("./upstream/data/store");
+  const paths = require("./data/paths");
 
-const want = paths.flatMap(path => jp.nodes(obj, path));
-const mp = new MultiPath();
-const got = [];
-for (const path of paths)
-  mp.addVisitor(path, (value, path) => got.push({ value, path }));
-mp.compile()(obj);
+  const want = paths.flatMap(path => jp.nodes(obj, path));
+  const mp = new MultiPath();
+  const got = [];
+  for (const path of paths)
+    mp.addVisitor(path, (value, path) => got.push({ value, path }));
+  mp.compile()(obj);
 
-tap.same(got, want, `MultiPath`);
+  tap.same(got, want, `MultiPath`);
+});
+
+tap.test(`mutator`, async () => {
+  const obj = {
+    page: {
+      body: {},
+      header: {
+        links: [
+          { url: "/", title: "home" },
+          { url: "/about", title: "about" }
+        ]
+      }
+    }
+  };
+
+  const want = {
+    page: {
+      body: {},
+      header: {
+        links: [
+          {
+            url: "https://example.com/",
+            title: "$.page.header.links[0].title"
+          },
+          {
+            url: "https://example.com/about",
+            title: "$.page.header.links[1].title"
+          }
+        ]
+      }
+    }
+  };
+
+  const mp = new MultiPath();
+  mp.addMutator("$..title", (value, path) => jp.stringify(path));
+  mp.addMutator(
+    "$..links[*].url",
+    (value, path) => "https://example.com" + value
+  );
+  mp.compile()(obj);
+  tap.same(obj, want, `mutated`);
+});
