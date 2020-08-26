d8bb4664c3525c5d767984c8e658f8ce9584663e
Andy Armstrong <andy@hexten.net>
2020-08-26 11:37:46 +0100

    Smarter code binding.

--- test/lastly.js
+++ test/lastly.js
@@ -1,22 +1,85 @@
 "use strict";
 
 const tap = require("tap");
-const { bindLastly } = require("../lib/lastly");
+const { bindFactory, bindLastly } = require("../lib/lastly");
 
 tap.test(`recursive`, async () => {
   const expr = bindLastly("@.pathString", {
-    path: () => "path",
-    pathString: () => "jp.stringify(@.path)"
+    path: () => ({ code: "path" }),
+    pathString: () => ({ code: "jp.stringify(@.path)" })
   });
-  tap.equals(expr, "jp.stringify(path)", `recursive resolution`);
+  tap.equals(expr, "jp.stringify(path);", `recursive resolution`);
 });
 
 tap.test(`syntax`, async () => {
   const expr = bindLastly("@.pathString.length", {
-    path: () => "path",
-    pathString: () => "jp.stringify(@.path)"
+    path: () => ({ code: "path" }),
+    pathString: () => ({ code: "jp.stringify(@.path)" })
   });
-  tap.equals(expr, "jp.stringify(path).length", `syntax`);
+  tap.equals(expr, "jp.stringify(path).length;", `syntax`);
+});
+
+const extractTracer = code => {
+  const out = [];
+  for (const line of code.split(/\n/)) {
+    const m = line.match(/(\w+\s+tracer\d+)/);
+    if (m) out.push(m[1]);
+  }
+  return out;
+};
+
+tap.test(`binding`, async () => {
+  const ids = {};
+  const ctx = {
+    sym(name) {
+      const next = (ids[name] = (ids[name] || 0) + 1);
+      return [name + next];
+    },
+
+    lval() {
+      return "obj";
+    }
+  };
+
+  const factory = bindFactory(ctx);
+
+  const context = {
+    value: () => ({ code: ctx.lval() }),
+    path: () => {
+      return factory("path", v => `var ${v} = stack.slice(0);`);
+    },
+    tracer: () => {
+      const [name] = ctx.sym("tracer");
+      return {
+        pre: `console.log("init ${name}");`,
+        post: `console.log("teardown ${name}");`,
+        code: `console.log("use ${name}");`
+      };
+    }
+  };
+
+  // vim thinks a bare brace is an object literal
+  if (1) {
+    const expr = bindLastly("@.tracer; @.tracer; @.tracer;", context);
+    const got = extractTracer(expr);
+    const want = [
+      "init tracer1",
+      "init tracer2",
+      "init tracer3",
+      "use tracer1",
+      "use tracer2",
+      "use tracer3",
+      "teardown tracer3",
+      "teardown tracer2",
+      "teardown tracer1"
+    ];
+    tap.same(got, want, `post called in reverse order`);
+  }
+
+  if (1) {
+    const expr = bindLastly("callback(@.path, @.path);", context);
+    tap.same(expr.match(/\bslice\b/g), ["slice"], `stack eval only once`);
+  }
 });
 
 tap.test(`negative`, async () => {
--- tools/mule.js
+++ tools/mule.js
@@ -1,26 +1,22 @@
 "use strict";
 
-function JSONPath() {
-  const self = function(template, ...args) {
-    return {
-      spill() {
-        return { template, args };
-      }
-    };
+const _ = require("lodash");
+const { bindFactory, bindLastly } = require("../lib/lastly");
+
+const compileLastly = code => {
+  const factory = bindFactory();
+  const ctx = {
+    path: () => factory("path", v => `var ${v} = stack.slice(0);`),
+    pathString: () =>
+      factory("pathString", v => `var ${v} = jp.stringify(@.path)`)
   };
 
-  self.apply = function() {
-    console.log("Apply!");
-  };
+  const expr = bindLastly(code, ctx);
+  return expr;
+};
 
-  return self;
-}
+const expr = compileLastly(
+  "if (@.pathString.length > 3) console.log(`Long ${@.pathString}`);"
+);
 
-const jp = new JSONPath();
-
-const i = 0,
-  j = 1;
-
-const x = jp`$.slot[${i}][${j}]`.spill();
-console.log(x);
-jp.apply();
+console.log(expr);
