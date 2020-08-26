10abfefdcbfa230880eb941c4c547b09de6aef7b
Andy Armstrong <andy@hexten.net>
2020-08-26 16:37:57 +0100

    Correctly handle context options in forked tree.

--- test/multipath.js
+++ test/multipath.js
@@ -19,70 +19,101 @@ tap.test(`conformance`, async () => {
 });
 
 tap.test(`MultiPath`, async () => {
-  const obj = {
-    page: {
-      body: { title: "All about me!" },
-      header: {
-        links: [
-          { url: "/", title: "home" },
-          { url: "/about", title: "about" }
-        ]
+  tap.test(`Visitors and mutators`, async () => {
+    const obj = {
+      page: {
+        body: { title: "All about me!" },
+        header: {
+          links: [
+            { url: "/", title: "home" },
+            { url: "/about", title: "about" }
+          ]
+        }
       }
-    }
-  };
+    };
 
-  const want = {
-    page: {
-      body: { title: "$.page.body.title" },
-      header: {
-        links: [
-          {
-            url: "https://example.com/",
-            title: "$.page.header.links[0].title"
-          },
-          {
-            url: "https://example.com/about",
-            title: "$.page.header.links[1].title"
-          }
-        ]
+    const want = {
+      page: {
+        body: { title: "$.page.body.title" },
+        header: {
+          links: [
+            {
+              url: "https://example.com/",
+              title: "$.page.header.links[0].title"
+            },
+            {
+              url: "https://example.com/about",
+              title: "$.page.header.links[1].title"
+            }
+          ]
+        }
       }
-    }
-  };
+    };
 
-  const mp = new MultiPath();
-  const before = [],
-    after = [];
+    const mp = new MultiPath();
+    const before = [],
+      after = [];
 
-  mp.addVisitor("$..title", (value, path) => before.push({ value, path }))
-    .addMutator("$..title", (value, path) => jp.stringify(path))
-    .addMutator(
-      "$..links[*].url",
-      (value, path) => "https://example.com" + value
-    )
-    .addVisitor("$..title", (value, path) => after.push({ value, path }));
+    mp.addVisitor("$..title", (value, path) => before.push({ value, path }))
+      .addMutator("$..title", (value, path) => jp.stringify(path))
+      .addMutator(
+        "$..links[*].url",
+        (value, path) => "https://example.com" + value
+      )
+      .addVisitor("$..title", (value, path) => after.push({ value, path }));
 
-  mp.compile()(obj);
+    const $ = {};
+    mp.compile()(obj, $);
 
-  tap.same(obj, want, `mutated`);
+    tap.same(obj, want, `mutated`);
 
-  const want2 = {
-    before: [
-      { value: "All about me!", path: ["$", "page", "body", "title"] },
-      { value: "home", path: ["$", "page", "header", "links", 0, "title"] },
-      { value: "about", path: ["$", "page", "header", "links", 1, "title"] }
-    ],
-    after: [
-      { value: "$.page.body.title", path: ["$", "page", "body", "title"] },
-      {
-        value: "$.page.header.links[0].title",
-        path: ["$", "page", "header", "links", 0, "title"]
+    const want2 = {
+      before: [
+        { value: "All about me!", path: ["$", "page", "body", "title"] },
+        { value: "home", path: ["$", "page", "header", "links", 0, "title"] },
+        { value: "about", path: ["$", "page", "header", "links", 1, "title"] }
+      ],
+      after: [
+        { value: "$.page.body.title", path: ["$", "page", "body", "title"] },
+        {
+          value: "$.page.header.links[0].title",
+          path: ["$", "page", "header", "links", 0, "title"]
+        },
+        {
+          value: "$.page.header.links[1].title",
+          path: ["$", "page", "header", "links", 1, "title"]
+        }
+      ]
+    };
+
+    tap.same({ before, after }, want2, `visit order preserved`);
+  });
+
+  tap.test(`Actions`, async () => {
+    const mp = new MultiPath();
+
+    mp.addAction("$.foo.bar", `$.log.push([@.value, @.path]);`)
+      .addAction("$.foo.baz", `$.log.push([@.value]);`)
+      .addAction("$.foo.bof[0].meta.control", `@.value = true`)
+      .addAction("$..*", `$.survey.push([@.pathString, @.leaf]);`);
+
+    const obj = { foo: { bar: "Bar!", baz: "Baz!" } };
+    const $ = { log: [], survey: [] };
+    mp.compile()(obj, $);
+
+    const want = {
+      obj: {
+        foo: { bar: "Bar!", baz: "Baz!", bof: [{ meta: { control: true } }] }
       },
-      {
-        value: "$.page.header.links[1].title",
-        path: ["$", "page", "header", "links", 1, "title"]
+      $: {
+        log: [["Bar!", ["$", "foo", "bar"]], ["Baz!"]],
+        survey: [
+          ["$.foo.bar", "Bar!"],
+          ["$.foo.baz", "Baz!"],
+          ["$.foo.bof[0].meta.control", true]
+        ]
       }
-    ]
-  };
-
-  tap.same({ before, after }, want2, `visit order preserved`);
+    };
+    tap.same({ obj, $ }, want, `addAction`);
+  });
 });
--- tools/mule.js
+++ tools/mule.js
@@ -1,18 +1,23 @@
 "use strict";
 
+const inspect = require("../lib/inspect");
 const jp = require("..");
 const prettier = require("prettier");
 
-const addTerminal = (path, lastly, ctx) =>
-  jp.compiler.compile(
-    [...jp.parse(path), { operation: "terminal", scope: "internal", lastly }],
-    ctx
-  );
+const { MultiPath } = require("../lib/multipath");
 
-const code = addTerminal("$..*", ctx => `cb(@.leaf);`, {
-  counted: true
+const mp = new MultiPath();
+
+mp.addAction("$.foo.bar", ctx => `console.log(@.value, @.path);`)
+  .addAction("$.foo.baz", ctx => `console.log(@.value)`)
+  .addAction("$.foo.bof[3].meta.control", ctx => `@.value = true`)
+  .addAction("$..*", ctx => `console.log(@.pathString, @.leaf);`);
+
+const pretty = prettier.format("module.exports = " + mp.code(), {
+  filepath: "code.js"
 });
-
-const pretty = prettier.format(code, { filepath: "code.js" });
-
 console.log(pretty);
+
+const obj = { foo: { bar: "Bar!", baz: "Baz!" } };
+mp.compile()(obj, {});
+console.log(inspect(obj));
