8cc6fa7ea2ff026249cab923fa02ad459bb65245
Andy Armstrong <andy@hexten.net>
2020-08-28 13:13:36 +0100

    Don't clone object.

--- test/pragmas.js
+++ test/pragmas.js
@@ -38,19 +38,19 @@ thing.apply({ hit: hit++ });
 
 const want = [
   { obj: { hit: 1 }, pragmas: {}, count: 1 },
-  { obj: { hit: 2 }, pragmas: {}, count: 1 },
+  { obj: { hit: 2 }, pragmas: {}, count: 2 },
   { obj: { hit: 3 }, pragmas: { inside: true }, count: 1 },
   { obj: { hit: 4 }, pragmas: { outside: true }, count: 1 },
-  { obj: { hit: 5 }, pragmas: { in: true }, count: 2 },
+  { obj: { hit: 5 }, pragmas: { in: true }, count: 3 },
   { obj: { hit: 6 }, pragmas: { inside: true, outside: true }, count: 1 },
   { obj: { hit: 7 }, pragmas: { outside: true }, count: 2 },
   { obj: { hit: 8 }, pragmas: { outside: true, inside: true }, count: 1 },
-  { obj: { hit: 9 }, pragmas: { out: true }, count: 2 },
+  { obj: { hit: 9 }, pragmas: { out: true }, count: 3 },
   { obj: { hit: 10 }, pragmas: { inside: true, outside: true }, count: 2 },
   { obj: { hit: 11 }, pragmas: { outside: true }, count: 3 },
-  { obj: { hit: 12 }, pragmas: { in: true }, count: 3 },
+  { obj: { hit: 12 }, pragmas: { in: true }, count: 4 },
   { obj: { hit: 13 }, pragmas: { inside: true }, count: 2 },
-  { obj: { hit: 14 }, pragmas: {}, count: 2 }
+  { obj: { hit: 14 }, pragmas: {}, count: 3 }
 ];
 
 tap.same(log, want, `pragmas`);
