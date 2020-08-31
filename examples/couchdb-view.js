b8266d94b3764d40d01d5d4fda28c84a8fa15b64
Andy Armstrong <andy@hexten.net>
2020-08-31 12:28:50 +0100

    More couch view stuff.

--- examples/couchdb-view.js
+++ examples/couchdb-view.js
@@ -3,19 +3,28 @@
 const jp = require("..");
 const prettier = require("prettier");
 
-const paths = [
-  "$..id",
-  "$.meta.control.issue",
-  "$.meta.control.auth",
-  "$.meta.edit.issue",
-  "$.meta.edit.object"
-];
+if (0) {
+  const paths = [
+    "$..id",
+    "$.meta.control.issue",
+    "$.meta.control.auth",
+    "$.meta.edit.issue",
+    "$.meta.edit.object"
+  ];
 
-const view = jp.nest().unordered;
-for (const path of paths) {
-  view.at(path, "emit([doc._id, -1, @.value]); emit([@.value, 1, doc._id]);");
+  const view = jp.nest().unordered;
+  for (const path of paths) {
+    view.at(path, "emit([doc._id, -1, @.value]); emit([@.value, 1, doc._id]);");
+  }
+
+  const code = view.generate();
+  const pretty = prettier.format(code, { filepath: "view.js" });
+  console.log(pretty);
 }
-
-const code = view.generate();
-const pretty = prettier.format(code, { filepath: "view.js" });
-console.log(pretty);
+if (1) {
+  const view = jp.nest().unordered;
+  view.string.leaf.at("$..*", "emit([@.path, JSON.stringify(@.value)]);");
+  const code = view.generate();
+  const pretty = prettier.format(code, { filepath: "view.js" });
+  console.log(pretty);
+}
