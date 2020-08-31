054e1f1ce308a4528e17ff05e29c22872adf9caf
Andy Armstrong <andy@hexten.net>
2020-08-31 15:24:14 +0100

    More tests

--- benchmark/nest.js
+++ benchmark/nest.js
@@ -11,52 +11,59 @@ function work(value, path) {
   // Nothing
 }
 
-function longhand(jp, count) {
+function longhand(jp, count, paths) {
   return function() {
     for (let i = 0; i < count; i++)
-      for (const searchPath of spec.paths)
+      for (const searchPath of paths)
         for (const { path, value } of jp.nodes(spec.obj, searchPath))
           work(value, jp.stringify(path));
   };
 }
 
-function baseline(count) {
-  return longhand(jpBase, count);
+function baseline(count, paths) {
+  return longhand(jpBase, count, paths);
 }
 
-function discreet(count) {
-  return longhand(jp, count);
+function discreet(count, paths) {
+  return longhand(jp, count, paths);
 }
 
-function nest(count) {
+function nest(count, paths) {
   const view = jp.nest().string.unordered;
-  for (const path of spec.paths) view.visitor(path, work);
+  for (const path of paths) view.visitor(path, work);
   return function() {
     for (let i = 0; i < count; i++) view(spec.obj);
   };
 }
 
+const tests = [
+  { name: "multipath", paths: spec.paths },
+  { name: "search", paths: ["$..*"] }
+];
+
 for (const count of [1, 10, 100, 1000]) {
-  const suite = new Benchmark.Suite();
-  const ar = _.range(count);
+  for (const { name, paths } of tests) {
+    const suite = new Benchmark.Suite();
+    const ar = _.range(count);
 
-  let x = 0;
-  const cb = (el, i, o) => (x = x + i);
-  suite
-    .add(`baseline ${count}`, baseline(count))
-    .add(`discreet ${count}`, discreet(count))
-    .add(`nest ${count}`, nest(count));
+    let x = 0;
+    const cb = (el, i, o) => (x = x + i);
+    suite
+      .add(`${name} baseline ${count}`, baseline(count, paths))
+      .add(`${name} discreet ${count}`, discreet(count, paths))
+      .add(`${name} nest ${count}`, nest(count, paths));
 
-  suite
-    .on("cycle", function(event) {
-      console.log(`//  ${event.target}`);
-    })
-    .on("complete", function() {
-      console.log("// Fastest is " + this.filter("fastest").map("name"));
-    })
-    .on("error", function(e) {
-      console.error(e);
-      process.exit(1);
-    })
-    .run();
+    suite
+      .on("cycle", function(event) {
+        console.log(`//  ${event.target}`);
+      })
+      .on("complete", function() {
+        console.log("// Fastest is " + this.filter("fastest").map("name"));
+      })
+      .on("error", function(e) {
+        console.error(e);
+        process.exit(1);
+      })
+      .run();
+  }
 }
