a7518b2e0cf906121ccb96c13800871c73952838
Andy Armstrong <andy@hexten.net>
2020-09-10 11:25:29 +0100

    Make sure deep viv works.

--- test/visit.js
+++ test/visit.js
@@ -7,3 +7,14 @@ tap.test("vivify root", async () => {
   const obj = jp.visit(undefined, "$", () => "Hello");
   tap.same(obj, "Hello", "vivified root");
 });
+
+tap.test("vivify deep node", async () => {
+  const doc = { id: "FOO" };
+  const out = jp.visit(doc, "$.meta.author", () => "andy@hexten.net");
+  tap.same(
+    doc,
+    { id: "FOO", meta: { author: "andy@hexten.net" } },
+    "doc updated"
+  );
+  tap.same(doc, out, "doc returned");
+});
