3da0da09c4ee2de74ff82c20d30c55e1ab890704
Andy Armstrong <andy@hexten.net>
2020-09-10 12:03:33 +0100

    Fix syntax error.

--- tools/mule.js
+++ tools/mule.js
@@ -5,17 +5,23 @@ const jp = require("..");
 const prettier = require("prettier");
 
 jp.compiler.on("compile", info => {
+  //  console.log(info.code);
   const pretty = prettier.format(info.code, { filepath: "code.js" });
   console.log(pretty);
 });
 
-var data = {
-  a: 1,
-  b: 2,
-  c: 3,
-  z: {
-    a: 100,
-    b: 200
-  }
+const nest = jp.nest().string;
+
+nest.mutator("$..id", value => value.toLowerCase());
+
+const doc = {
+  author: { id: "ABC", name: "Smoo" },
+  editor: { id: "DEF", name: "Pizzo!" }
 };
-var parent = jp.parent(data, "$.z.b");
+
+const want = {
+  author: { id: "abc", name: "Smoo" },
+  editor: { id: "def", name: "Pizzo!" }
+};
+
+const got = nest(doc);
